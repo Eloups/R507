c7d0f20fe08d55a16f3196dba0ee56c19331d291
Eloups <121400663+Eloups@users.noreply.github.com>
2025-10-07 16:43:36 +0200

    test: ajout de tests

---
+++ tests/MainControllerTest.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Tests;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class MainControllerTest extends WebTestCase
+{
+    public function testMain(): void
+    {
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/');
+
+        $this->assertResponseIsSuccessful();
+        $this->assertSelectorTextContains('h1', 'Formulaire de contact');
+    }
+
+    public function testMain2(): void
+    {
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/');
+
+        $this->assertResponseIsSuccessful();
+        $this->assertSelectorTextContains('h1', 'Formulaire de contact');
+    }
+
+    public function testMain3(): void
+    {
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/');
+
+        $this->assertResponseIsSuccessful();
+        $this->assertSelectorTextContains('h1', 'Formulaire de contact');
+    }
+
+    public function testMain4(): void
+    {
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/');
+
+        $this->assertResponseIsSuccessful();
+        $this->assertSelectorTextContains('h1', 'Formulaire de contact');
+    }
+
+    public function testMain5(): void
+    {
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/');
+
+        $this->assertResponseIsSuccessful();
+        $this->assertSelectorTextContains('h1', 'Formulaire de contact');
+    }
+}
